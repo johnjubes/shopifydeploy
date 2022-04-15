38be458d3ce56c5a831e52484851152ea36106db
johnjubes <63511106+johnjubes@users.noreply.github.com>
2022-04-15 15:51:40 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function Index() {
         <Layout>
           <EmptyState // Empty state component
             heading="Create Product Options"
-            action={{
-              content: 'Select products',
-              onAction: () => this.setState({ open: true }),
-            }}
+            
             image={img}
           >
-            <p>Select products to add options and variants.</p>
+            <p>Our app allows you to create as many product options as you need by treating every option as its own product. </p>
+            <p>To create a set of product options, first create a "base" product, and then an additional product for every option using shopify's default product maker. Feel free to include additional variants on all these products. </p>
+<p>Once you have all your options created, create a new "collection" under the same name as your base product and add all your newly created products to the collection.</p>
+<p>Now all the products in that collection will be displayed on the base product's page, customers will be able to add and remove options from their carts and see the price update as they do!</p>
           </EmptyState>
         </Layout>
       </Page>
